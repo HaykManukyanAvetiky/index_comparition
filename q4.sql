753c4551f4a783e717474865df4495612800742f
Hayk Manukyan <hmanukyan@findmypast.com>
2021-11-01 16:04:09 +0400

    adding random sqls

---
+++ q4.sql
@@ -0,0 +1,15 @@
+\set j1 random(0,9600)
+\set j2 (:j1 + 400)
+\set n1 random(0,4800)
+\set n2 (:n1 + 200)
+\set y1 random(0,160)
+\set y2 (:y1 + 40)
+\set s1 random(0,4800)
+\set s2 (:s1 + 200)
+
+select * from test_4_col_ind
+where
+job between :j1 and :j2
+and nlp between :n1 and :n2
+and year between :y1 and :y2
+and sequence between :s1 and :s2;
